cf92d61be02b181ca68d131012abf3cb2e69e49c
afadingthought <124819289+afadingthought@users.noreply.github.com>
2024-05-21 11:09:33 -0600

    Update LittleArcCMD.sh

--- yabai/scripts/LittleArcCMD.sh
+++ yabai/scripts/LittleArcCMD.sh
@@ -8,21 +8,24 @@
 window_id=$YABAI_WINDOW_ID
 myaction=$1
 
+desktop_path="$HOME/Desktop/debug.txt"
+#echo "works1" > "$desktop_path"
+
 # Query Yabai for window information
 window_info=$(/opt/homebrew/bin/yabai -m query --windows --window "$window_id")
 # Extract the app, title, and whether the window is floating
 app=$(echo "$window_info" | /opt/homebrew/bin/jq -r '.app')
-layer=$(echo "$window_info" | /opt/homebrew/bin/jq -r '.layer')
-is_floating=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."is-floating"')
+sublayer=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."sub-layer"')
+isfloating=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."is-floating"')
 # Check if app is Arc, layer is normal, and window is floating - will leave it floating (yabaiwindow tag)
-if [[ "$app" == "Arc" && "$layer" == "normal" && "$is_floating" == "true" && "$myaction" == "floatcenter" ]]; then
+if [[ "$app" == "Arc" && "$sublayer" == "normal" && "$isfloating" == "true" && "$myaction" == "floatcenter" ]]; then
     # Toggle float on the window
     /opt/homebrew/bin/yabai -m window "$window_id" --grid 6:4:1:1:2:4
     /opt/homebrew/bin/yabai -m signal --remove littlearccmd
-    /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Library/CloudStorage/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
-elif [[ "$app" == "Arc" && "$layer" == "normal" && "$is_floating" == "true" && "$myaction" == "tile" ]]; then
+    /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
+elif [[ "$app" == "Arc" && "$sublayer" == "normal" && "$isfloating" == "true" && "$myaction" == "tile" ]]; then
    # Toggle float on the window
     /opt/homebrew/bin/yabai -m window "$window_id" --toggle float
     /opt/homebrew/bin/yabai -m signal --remove littlearccmd
-    /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Library/CloudStorage/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
+    /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
 fi
