3e9112556942b749bd14ec78b1830d6fd21aac1c
afadingthought <124819289+afadingthought@users.noreply.github.com>
2024-05-21 11:09:59 -0600

    Update LittleArc.sh

--- yabai/scripts/LittleArc.sh
+++ yabai/scripts/LittleArc.sh
@@ -2,18 +2,25 @@
 # Get the window id from the argument
 window_id=$YABAI_WINDOW_ID
 
+# Set the path to the desktop
+desktop_path="$HOME/Desktop/debug.txt"
+#echo "works" > "$desktop_path"
+
 # Query Yabai for window information
 window_info=$(/opt/homebrew/bin/yabai -m query --windows --window "$window_id")
 # Extract the app, title, and whether the window is floating
 app=$(echo "$window_info" | /opt/homebrew/bin/jq -r '.app')
-layer=$(echo "$window_info" | /opt/homebrew/bin/jq -r '.layer')
-is_floating=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."is-floating"')
+sublayer=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."sub-layer"')
+isfloating=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."is-floating"')
+
 # Check if app is Arc, layer is normal, and window is floating - will leave it floating (yabaiwindow tag)
-if [[ "$app" == "Arc" && "$layer" == "normal" && "$is_floating" == "true" ]]; then
+if [[ "$app" == "Arc" && "$sublayer" == "normal" && "$isfloating" == "true" ]]; then
     # Toggle float on the window. NO LONGER NECESSARY since everything is normal?
-    # /opt/homebrew/bin/yabai -m window "$window_id" --layer below
+    # /opt/homebrew/bin/yabai -m window "$window_id" --sub-layer below
+    :
 # Check if app is Arc, layer is above, and window is floating - will tile it (yabaitile tag)
-elif [[ "$app" == "Arc" && "$layer" == "above" && "$is_floating" == "true" ]]; then
+elif [[ "$app" == "Arc" && "$sublayer" == "above" && "$isfloating" == "true" ]]; then
+    # Create the test.txt file with "hey" as content
     sleep .3
-    /opt/homebrew/bin/yabai -m window "$window_id" --toggle float --layer normal
+    /opt/homebrew/bin/yabai -m window "$window_id" --toggle float --sub-layer normal
 fi
