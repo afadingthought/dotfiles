2f2aaaab6b6dca2beb5b0f689b7b1446aaf9f100
afadingthought <124819289+afadingthought@users.noreply.github.com>
2024-05-24 12:16:19 -0600

    Update LittleArcCMD.sh

--- yabai/scripts/LittleArcCMD.sh
+++ yabai/scripts/LittleArcCMD.sh
@@ -8,8 +8,8 @@
 window_id=$YABAI_WINDOW_ID
 myaction=$1
 
-desktop_path="$HOME/Desktop/debug.txt"
-#echo "works1" > "$desktop_path"
+#desktop_path="$HOME/Desktop/debug.txt"
+#echo "works" > "$desktop_path"
 
 # Query Yabai for window information
 window_info=$(/opt/homebrew/bin/yabai -m query --windows --window "$window_id")
@@ -19,13 +19,13 @@ sublayer=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."sub-layer"')
 isfloating=$(echo "$window_info" | /opt/homebrew/bin/jq -r '."is-floating"')
 # Check if app is Arc, layer is normal, and window is floating - will leave it floating (yabaiwindow tag)
 if [[ "$app" == "Arc" && "$sublayer" == "normal" && "$isfloating" == "true" && "$myaction" == "floatcenter" ]]; then
-    # Toggle float on the window
+    # Toggle float on the window and restores original Arc signal
     /opt/homebrew/bin/yabai -m window "$window_id" --grid 6:4:1:1:2:4
     /opt/homebrew/bin/yabai -m signal --remove littlearccmd
-    /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
+    # /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
 elif [[ "$app" == "Arc" && "$sublayer" == "normal" && "$isfloating" == "true" && "$myaction" == "tile" ]]; then
-   # Toggle float on the window
+    # Toggle float on the window and restores original Arc signal
     /opt/homebrew/bin/yabai -m window "$window_id" --toggle float
     /opt/homebrew/bin/yabai -m signal --remove littlearccmd
-    /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
+    # /opt/homebrew/bin/yabai -m signal  --add label="littlearc" app='^Arc$' event=window_title_changed action="~/Dropbox/2-Areas/SystemAndApps/Yabai/LittleArc.sh"
 fi
